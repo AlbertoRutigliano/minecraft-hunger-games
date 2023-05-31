5be3806a2b9743eb1a51aa0c108e57989363f1a7
rutiglianoalberto97@gmail.com <rutiglianoalberto97@gmail.com>
2023-05-31 22:52:20 +0200

    initial commit

---
+++ src/main/java/it/lar/minecraft/hg/App.java
@@ -0,0 +1,13 @@
+package it.lar.minecraft.hg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
